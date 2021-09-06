60394f06133d8f8037c8f19bb4ecbc0e3ef893e5
Xueying Luo <snowvan113@gmail.com>
2021-09-05 23:06:21 -0600

    modified script

--- lab1.py
+++ lab1.py
@@ -1,6 +1,12 @@
 import requests 
 
+print("\nRequests library version:")
 print(requests.__version__)
 
+print("\nGet google webpage:")
 page = requests.get('http://google.com/')
 print(page)
+
+print("\nPython script:\n")
+script = requests.get('https://raw.githubusercontent.com/xyL3/404Lab1/main/lab1.py')
+print(script.text)
